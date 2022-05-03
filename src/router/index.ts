1915733a9c0a25d832db9a26a5fb1810e7bf96a9
perlinleo <perlinleo@github.com>
2022-05-03 19:19:01 +0300

    wussup

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,6 +34,68 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "sign-in" */ "../views/LoginView.vue"),
   },
+  {
+    path: "/declaration",
+    name: "declaration",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "sign-in" */ "../views/DeclarationUserView.vue"
+      ),
+  },
+  {
+    path: "/pass",
+    name: "pass",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "sign-in" */ "../views/PassesUserView.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "sign-in" */ "../views/ProfileUserView.vue"),
+  },
+  {
+    path: "/passages",
+    name: "passages",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "sign-in" */ "../views/PassagesAdminView.vue"
+      ),
+  },
+  {
+    path: "/declarations",
+    name: "declarations",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "sign-in" */ "../views/DeclarationAdminView.vue"
+      ),
+  },
+  {
+    path: "/profiles",
+    name: "profiles",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "sign-in" */ "../views/ProfilesAdminView.vue"
+      ),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/PassagesAdminView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>проходы</h1>
+  </div>
+</template>
